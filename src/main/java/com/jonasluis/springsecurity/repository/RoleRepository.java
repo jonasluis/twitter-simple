70d3914708af9d07e1c516b6a32441838180d78f
jonasluis <jonasluis66@gmail.com>
2025-05-31 17:04:10 -0300

    feat: Criar repositorios

---
+++ src/main/java/com/jonasluis/springsecurity/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.jonasluis.springsecurity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jonasluis.springsecurity.entities.Role;
+
+@Repository
+public interface RoleRepository  extends JpaRepository<Role,  Long>{
+    
+}
---
+++ src/main/java/com/jonasluis/springsecurity/repository/TweetRepository.java
@@ -0,0 +1,10 @@
+package com.jonasluis.springsecurity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.jonasluis.springsecurity.entities.Tweet;
+
+@Repository
+public interface TweetRepository  extends JpaRepository<Tweet,  Long>{
+    
+}
